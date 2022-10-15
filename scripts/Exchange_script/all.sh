e68ebde0cb475741980ace8eccff706bb212b5ac
Luis Ramirez <luis.ramirezsolis@cinvestav.mx>
2022-10-15 00:30:07 -0500

    all models test script

---
+++ scripts/Exchange_script/all.sh
@@ -0,0 +1,79 @@
+python -u run.py \
+  --is_training 1 \
+  --root_path /datasets/Exchange_rate \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model Autoformer \
+  --data custom \
+  --features S \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 1 \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path /datasets/Exchange_rate \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model Informer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1
+
+  python -u run.py \
+  --is_training 1 \
+  --root_path /datasets/Exchange_rate \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model Reformer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1
+
+  python -u run.py \
+  --is_training 1 \
+  --root_path /datasets/Exchange_rate \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model Transformer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1
